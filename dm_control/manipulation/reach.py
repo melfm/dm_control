e07fb1d5430f8112eb7022e6006243992deb78aa
Melissa Mozifian <melissafm24@gmail.com>
2020-09-23 15:34:27 -0400

    change reach reward, reformatting

--- dm_control/manipulation/reach.py
+++ dm_control/manipulation/reach.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ============================================================================
-
 """A task where the goal is to move the hand close to a target prop or site."""
 
 from __future__ import absolute_import
@@ -37,7 +36,6 @@ from dm_control.manipulation.shared import workspaces
 from dm_control.utils import rewards
 import numpy as np
 
-
 _ReachWorkspace = collections.namedtuple(
     '_ReachWorkspace', ['target_bbox', 'tcp_bbox', 'arm_offset'])
 
@@ -45,32 +43,38 @@ _ReachWorkspace = collections.namedtuple(
 _PROP_Z_OFFSET = 0.001
 
 _DUPLO_WORKSPACE = _ReachWorkspace(
-    target_bbox=workspaces.BoundingBox(
-        lower=(-0.1, -0.1, _PROP_Z_OFFSET),
-        upper=(0.1, 0.1, _PROP_Z_OFFSET)),
-    tcp_bbox=workspaces.BoundingBox(
-        lower=(-0.1, -0.1, 0.2),
-        upper=(0.1, 0.1, 0.4)),
+    target_bbox=workspaces.BoundingBox(lower=(-0.1, -0.1, _PROP_Z_OFFSET),
+                                       upper=(0.1, 0.1, _PROP_Z_OFFSET)),
+    tcp_bbox=workspaces.BoundingBox(lower=(-0.1, -0.1, 0.2),
+                                    upper=(0.1, 0.1, 0.4)),
     arm_offset=robots.ARM_OFFSET)
 
+# Fixing the target for single-goal reach
+# X:Left/Right+   Y:Up/Down   Z:Forward/Backward
+# _SITE_WORKSPACE = _ReachWorkspace(
+#     target_bbox=workspaces.BoundingBox(
+#         lower=(0.23, 0.12, 0.02),
+#         upper=(0.25, 0.15, 0.05)),
+#     tcp_bbox=workspaces.BoundingBox(
+#         lower=(0.1, 0.1, 0.02),
+#         upper=(0.1, 0.1, 0.02)),
+#     arm_offset=robots.ARM_OFFSET)
+
 _SITE_WORKSPACE = _ReachWorkspace(
-    target_bbox=workspaces.BoundingBox(
-        lower=(-0.2, -0.2, 0.02),
-        upper=(0.2, 0.2, 0.4)),
-    tcp_bbox=workspaces.BoundingBox(
-        lower=(-0.2, -0.2, 0.02),
-        upper=(0.2, 0.2, 0.4)),
+    target_bbox=workspaces.BoundingBox(lower=(-0.2, -0.2, 0.02),
+                                       upper=(0.2, 0.2, 0.4)),
+    tcp_bbox=workspaces.BoundingBox(lower=(-0.2, -0.2, 0.02),
+                                    upper=(0.2, 0.2, 0.4)),
     arm_offset=robots.ARM_OFFSET)
 
 _TARGET_RADIUS = 0.05
 
 
 class Reach(composer.Task):
-  """Bring the hand close to a target prop or site."""
-
-  def __init__(
-      self, arena, arm, hand, prop, obs_settings, workspace, control_timestep):
-    """Initializes a new `Reach` task.
+    """Bring the hand close to a target prop or site."""
+    def __init__(self, arena, arm, hand, prop, obs_settings, workspace,
+                 control_timestep):
+        """Initializes a new `Reach` task.
 
     Args:
       arena: `composer.Entity` instance.
@@ -83,91 +87,112 @@ class Reach(composer.Task):
       workspace: `_ReachWorkspace` specifying the placement of the prop and TCP.
       control_timestep: Float specifying the control timestep in seconds.
     """
-    self._arena = arena
-    self._arm = arm
-    self._hand = hand
-    self._arm.attach(self._hand)
-    self._arena.attach_offset(self._arm, offset=workspace.arm_offset)
-    self.control_timestep = control_timestep
-    self._tcp_initializer = initializers.ToolCenterPointInitializer(
-        self._hand, self._arm,
-        position=distributions.Uniform(*workspace.tcp_bbox),
-        quaternion=workspaces.DOWN_QUATERNION)
+        self._arena = arena
+        self._arm = arm
+        self._hand = hand
+        self._arm.attach(self._hand)
+        self._arena.attach_offset(self._arm, offset=workspace.arm_offset)
+        self.control_timestep = control_timestep
+        self._tcp_initializer = initializers.ToolCenterPointInitializer(
+            self._hand,
+            self._arm,
+            position=distributions.Uniform(*workspace.tcp_bbox),
+            quaternion=workspaces.DOWN_QUATERNION)
 
-    # Add custom camera observable.
-    self._task_observables = cameras.add_camera_observables(
-        arena, obs_settings, cameras.FRONT_CLOSE)
+        # Add custom camera observable.
+        self._task_observables = cameras.add_camera_observables(
+            arena, obs_settings, cameras.FRONT_CLOSE)
 
-    target_pos_distribution = distributions.Uniform(*workspace.target_bbox)
-    self._prop = prop
-    if prop:
-      # The prop itself is used to visualize the target location.
-      self._make_target_site(parent_entity=prop, visible=False)
-      self._target = self._arena.add_free_entity(prop)
-      self._prop_placer = initializers.PropPlacer(
-          props=[prop],
-          position=target_pos_distribution,
-          quaternion=workspaces.uniform_z_rotation,
-          settle_physics=True)
-    else:
-      self._target = self._make_target_site(parent_entity=arena, visible=True)
-      self._target_placer = target_pos_distribution
+        target_pos_distribution = distributions.Uniform(*workspace.target_bbox)
+        self._prop = prop
+        if prop:
+            # The prop itself is used to visualize the target location.
+            self._make_target_site(parent_entity=prop, visible=False)
+            self._target = self._arena.add_free_entity(prop)
+            self._prop_placer = initializers.PropPlacer(
+                props=[prop],
+                position=target_pos_distribution,
+                quaternion=workspaces.uniform_z_rotation,
+                settle_physics=True)
+        else:
+            self._target = self._make_target_site(parent_entity=arena,
+                                                  visible=True)
+            self._target_placer = target_pos_distribution
 
-      obs = observable.MJCFFeature('pos', self._target)
-      obs.configure(**obs_settings.prop_pose._asdict())
-      self._task_observables['target_position'] = obs
+            obs = observable.MJCFFeature('pos', self._target)
+            obs.configure(**obs_settings.prop_pose._asdict())
+            self._task_observables['target_position'] = obs
 
-    # Add sites for visualizing the prop and target bounding boxes.
-    workspaces.add_bbox_site(
-        body=self.root_entity.mjcf_model.worldbody,
-        lower=workspace.tcp_bbox.lower, upper=workspace.tcp_bbox.upper,
-        rgba=constants.GREEN, name='tcp_spawn_area')
-    workspaces.add_bbox_site(
-        body=self.root_entity.mjcf_model.worldbody,
-        lower=workspace.target_bbox.lower, upper=workspace.target_bbox.upper,
-        rgba=constants.BLUE, name='target_spawn_area')
+        # Add sites for visualizing the prop and target bounding boxes.
+        workspaces.add_bbox_site(body=self.root_entity.mjcf_model.worldbody,
+                                 lower=workspace.tcp_bbox.lower,
+                                 upper=workspace.tcp_bbox.upper,
+                                 rgba=constants.GREEN,
+                                 name='tcp_spawn_area')
+        workspaces.add_bbox_site(body=self.root_entity.mjcf_model.worldbody,
+                                 lower=workspace.target_bbox.lower,
+                                 upper=workspace.target_bbox.upper,
+                                 rgba=constants.BLUE,
+                                 name='target_spawn_area')
 
-  def _make_target_site(self, parent_entity, visible):
-    return workspaces.add_target_site(
-        body=parent_entity.mjcf_model.worldbody,
-        radius=_TARGET_RADIUS, visible=visible,
-        rgba=constants.RED, name='target_site')
+    def _make_target_site(self, parent_entity, visible):
+        return workspaces.add_target_site(
+            body=parent_entity.mjcf_model.worldbody,
+            radius=_TARGET_RADIUS,
+            visible=visible,
+            rgba=constants.RED,
+            name='target_site')
 
-  @property
-  def root_entity(self):
-    return self._arena
+    @property
+    def root_entity(self):
+        return self._arena
 
-  @property
-  def arm(self):
-    return self._arm
+    @property
+    def arm(self):
+        return self._arm
 
-  @property
-  def hand(self):
-    return self._hand
+    @property
+    def hand(self):
+        return self._hand
 
-  @property
-  def task_observables(self):
-    return self._task_observables
+    @property
+    def task_observables(self):
+        return self._task_observables
 
-  def get_reward(self, physics):
-    hand_pos = physics.bind(self._hand.tool_center_point).xpos
-    target_pos = physics.bind(self._target).xpos
-    distance = np.linalg.norm(hand_pos - target_pos)
-    return rewards.tolerance(
-        distance, bounds=(0, _TARGET_RADIUS), margin=_TARGET_RADIUS)
+    # def get_reward_sparse(self, physics):
+    #   hand_pos = physics.bind(self._hand.tool_center_point).xpos
+    #   target_pos = physics.bind(self._target).xpos
+    #   distance = np.linalg.norm(hand_pos - target_pos)
+    #   return rewards.tolerance(
+    #       distance, bounds=(0, _TARGET_RADIUS), margin=_TARGET_RADIUS)
 
-  def initialize_episode(self, physics, random_state):
-    self._hand.set_grasp(physics, close_factors=random_state.uniform())
-    self._tcp_initializer(physics, random_state)
-    if self._prop:
-      self._prop_placer(physics, random_state)
-    else:
-      physics.bind(self._target).pos = (
-          self._target_placer(random_state=random_state))
+    def get_reward(self, physics):
+      c1 = 1000
+      c2 = 0.01
+      c3 = 0.001
+      hand_pos = physics.bind(self._hand.tool_center_point).xpos
+      target_pos = physics.bind(self._target).xpos
+      distance = np.linalg.norm(hand_pos - target_pos)
+      reachRew = c1 * (self.maxReachDist - distance) + c1 * (
+          np.exp(-(distance**2) / c2) + np.exp(-(distance**2) / c3))
+      reachRew = max(reachRew, 0)
+      return reachRew
+
+    def initialize_episode(self, physics, random_state):
+        self._hand.set_grasp(physics, close_factors=random_state.uniform())
+        self._tcp_initializer(physics, random_state)
+        init_hand_pos = physics.bind(self._hand.tool_center_point).xpos
+        self.target_pos = physics.bind(self._target).xpos
+        self.maxReachDist = np.linalg.norm(init_hand_pos - self.target_pos)
+        if self._prop:
+            self._prop_placer(physics, random_state)
+        else:
+            physics.bind(self._target).pos = (self._target_placer(
+                random_state=random_state))
 
 
 def _reach(obs_settings, use_site):
-  """Configure and instantiate a `Reach` task.
+    """Configure and instantiate a `Reach` task.
 
   Args:
     obs_settings: An `observations.ObservationSettings` instance.
@@ -177,38 +202,41 @@ def _reach(obs_settings, use_site):
   Returns:
     An instance of `reach.Reach`.
   """
-  arena = arenas.Standard()
-  arm = robots.make_arm(obs_settings=obs_settings)
-  hand = robots.make_hand(obs_settings=obs_settings)
-  if use_site:
-    workspace = _SITE_WORKSPACE
-    prop = None
-  else:
-    workspace = _DUPLO_WORKSPACE
-    prop = props.Duplo(observable_options=observations.make_options(
-        obs_settings, observations.FREEPROP_OBSERVABLES))
-  task = Reach(arena=arena, arm=arm, hand=hand, prop=prop,
-               obs_settings=obs_settings,
-               workspace=workspace,
-               control_timestep=constants.CONTROL_TIMESTEP)
-  return task
+    arena = arenas.Standard()
+    arm = robots.make_arm(obs_settings=obs_settings)
+    hand = robots.make_hand(obs_settings=obs_settings)
+    if use_site:
+        workspace = _SITE_WORKSPACE
+        prop = None
+    else:
+        workspace = _DUPLO_WORKSPACE
+        prop = props.Duplo(observable_options=observations.make_options(
+            obs_settings, observations.FREEPROP_OBSERVABLES))
+    task = Reach(arena=arena,
+                 arm=arm,
+                 hand=hand,
+                 prop=prop,
+                 obs_settings=obs_settings,
+                 workspace=workspace,
+                 control_timestep=constants.CONTROL_TIMESTEP)
+    return task
 
 
 @registry.add(tags.FEATURES, tags.EASY)
 def reach_duplo_features():
-  return _reach(obs_settings=observations.PERFECT_FEATURES, use_site=False)
+    return _reach(obs_settings=observations.PERFECT_FEATURES, use_site=False)
 
 
 @registry.add(tags.VISION, tags.EASY)
 def reach_duplo_vision():
-  return _reach(obs_settings=observations.VISION, use_site=False)
+    return _reach(obs_settings=observations.VISION, use_site=False)
 
 
 @registry.add(tags.FEATURES, tags.EASY)
 def reach_site_features():
-  return _reach(obs_settings=observations.PERFECT_FEATURES, use_site=True)
+    return _reach(obs_settings=observations.PERFECT_FEATURES, use_site=True)
 
 
 @registry.add(tags.VISION, tags.EASY)
 def reach_site_vision():
-  return _reach(obs_settings=observations.VISION, use_site=True)
+    return _reach(obs_settings=observations.VISION, use_site=True)
